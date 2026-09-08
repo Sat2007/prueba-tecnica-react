e92629f9b4561b9c4f1e06a13e363c810d46c184
Sat2007 <santiagotibasozaa@gmail.com>
2026-09-08 16:35:54 -0500

    Cleaned code

--- src/providers/ReactQueryProvider.tsx
+++ src/providers/ReactQueryProvider.tsx
@@ -1,13 +1,11 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { useState } from "react";
 
 export default function ReactQueryProvider({
     children
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    //const [queryClient] = useState(() => new QueryClient())
     const queryClient = new QueryClient()
     return (
         <QueryClientProvider client={queryClient}>
